ba29c1e30a5101be6eb6938a563ef46bc88bd1ac
codewithsushantrimal <sushantrjrimal@gmail.com>
2023-08-14 18:11:23 +0545

    Working on Database

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const users = [];
 // Single routing
 const app = express();
 
 //database connection
 mongoose
-  .connect(" mongodb://127.0.0.1:27017/", {
+  .connect("mongodb://127.0.0.1:27017/", {
     dbName: "explorebackend",
   })
-  .then((c) => console.log("Database connected successfully!"))
+  .then(() => console.log("Database connected successfully"))
   .catch((e) => console.log("e"));
 
+// User Schema
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+});
+
+const Message = mongoose.model("Message", contactSchema);
 //view engine
 app.set("view engine", "ejs");
 
@@ -22,12 +30,26 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join("public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/add", async (req, res) => {
+  await Message.create({
+    name: "Sushant",
+    email: "devcommunitynepal@gmail.com",
+    message: "Hello, this is a test!",
+  });
+  res.send("hello!");
+});
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/contact", (req, res) => {
-  console.log(req.body);
+app.post("/contact", async (req, res) => {
+  await Message.create({
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message,
+  });
+
   res.redirect("/success");
 });
 
